3d6b00337d5197dd1ed5de1f39586e457f71158f
Martina Juri <martina.juri@mi.unc.edu.ar>
2024-07-10 14:30:31 -0300

    clear

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import {createPool} from 'mysql2/promise';
-export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Esc4l0n3t4',
-    port: 3306,
-    database: 'stock'
-});
-
