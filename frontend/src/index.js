16b6faa563ef60c3ea48deaf1a9d64bdf4ed83e8
Martina Juri <martina.juri@mi.unc.edu.ar>
2024-09-03 12:40:04 -0300

    Update index.js to use dotenv for environment variable configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Paso 1: Importar BrowserRouter
 import App from './App';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
