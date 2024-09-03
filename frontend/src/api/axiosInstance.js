de18ed99c73b6f797c81094bc27ea96391fc8c97
Martina Juri <martina.juri@mi.unc.edu.ar>
2024-09-03 12:43:03 -0300

    Refactor axiosInstance.js to remove unnecessary code

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 // Variable de entorno REACT_APP_API_URL
 const BACKEND_URL = process.env.REACT_APP_API_URL;
 
-// Crea una instancia de axios con configuración predeterminada
 const axiosInstance = axios.create({
   baseURL: BACKEND_URL,
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Paso 1: Importar BrowserRouter
 import App from './App';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
