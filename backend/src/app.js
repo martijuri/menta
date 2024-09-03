832ce37e12bf80a487f090f20edd6007f743281c
Martina Juri <martina.juri@mi.unc.edu.ar>
2024-09-03 18:20:05 -0300

    Refactor Dockerfile and nginx.conf to use environment variables for port and domain configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,14 +19,6 @@ app.get('/test-db', async (req, res) => {
   }
 });
 
-
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-console.log('DB_PORT:', process.env.DB_PORT);
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
